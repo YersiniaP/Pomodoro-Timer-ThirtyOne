32f4ae3fec33fac81e57db1801d4692fae3e66e2
shawn <shahzadshawn@gmail.com>
2020-08-09 13:44:40 -0700

    user is able to type in a valid email and reset their password

---
+++ app/src/main/java/com/example/pomodoro_app/PasswordRecovery.java
@@ -0,0 +1,72 @@
+package com.example.pomodoro_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import android.os.Parcelable;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.content.Intent;
+import android.widget.Button;
+import android.os.Bundle;
+import android.view.View;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseError;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class PasswordRecovery extends AppCompatActivity {
+
+    EditText emailAddress;
+    Button btn_send;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_recovery);
+        auth = FirebaseAuth.getInstance();
+        emailAddress = (EditText) findViewById(R.id.user_email_password_recovery);
+
+
+        btn_send = (Button) findViewById(R.id.send);
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = emailAddress.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter your email!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Sent!", Toast.LENGTH_LONG).show();
+                                    finish();
+
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "No email registered", Toast.LENGTH_LONG).show();
+                                }
+                            }
+
+                        });
+
+            }//end onClick
+        });
+
+
+    }// end onCreate
+
+}// end class passwordRecovery
